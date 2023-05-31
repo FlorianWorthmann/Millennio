76eceb303cd3c667a1fce6f4bca801900b16b546
pbt3h21awo <Florian.Worthmann@edu.bib.de>
2023-05-31 08:58:36 +0200

    update

---
+++ VPR_Millennio/VPR_Millennio/Markt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPR_Millennio
+{
+    internal class Markt
+    {
+        public void kaufen()
+        {
+
+        }
+
+        public void verkaufen()
+        {
+
+        }
+    }
+}
